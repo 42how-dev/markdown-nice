b317d5ce14033c46165e65eaa8103ff223738f69
tech-zjf <907572438@qq.com>
2023-04-25 15:02:40 +0800

    feat: 缓存问题

--- src/store/content.js
+++ src/store/content.js
@@ -26,7 +26,7 @@ class Content {
   @action
   setContent = (content) => {
     this.content = content;
-    window.localStorage.setItem(CONTENT, content);
+    // window.localStorage.setItem(CONTENT, content);
   };
 
   @action
